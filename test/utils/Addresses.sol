49495ac3ad8cfeeb0ba81b100b977c7a86efb804
alephao <7674479+alephao@users.noreply.github.com>
2022-11-12 14:10:27 -0300

    refactor: format

--- test/utils/Addresses.sol
+++ test/utils/Addresses.sol
@@ -3,23 +3,23 @@ pragma solidity >=0.8.4;
 
 // solhint-disable const-name-snakecase
 library Addresses {
-  address constant public deployer = address(uint160(0xd39101e5));
-  address constant public alephao = address(uint160(0xa1398a0));
-  address constant public cassie = address(uint160(0xca551e));
-  address constant public sofia = address(uint160(0x50F1a));
-  address constant public bob = address(uint160(0xb0b));
-  address constant public bia = address(uint160(0xb1a));
-  address constant public baboo = address(uint160(0xbab00));
+    address public constant deployer = address(uint160(0xd39101e5));
+    address public constant alephao = address(uint160(0xa1398a0));
+    address public constant cassie = address(uint160(0xca551e));
+    address public constant sofia = address(uint160(0x50F1a));
+    address public constant bob = address(uint160(0xb0b));
+    address public constant bia = address(uint160(0xb1a));
+    address public constant baboo = address(uint160(0xbab00));
 }
 
 // Shortcut
 library A {
-  address constant public deployer = address(uint160(0xd39101e5));
-  address constant public alephao = address(uint160(0xa1398a0));
-  address constant public cassie = address(uint160(0xca551e));
-  address constant public sofia = address(uint160(0x50F1a));
-  address constant public bob = address(uint160(0xb0b));
-  address constant public bia = address(uint160(0xb1a));
-  address constant public baboo = address(uint160(0xbab00));
+    address public constant deployer = address(uint160(0xd39101e5));
+    address public constant alephao = address(uint160(0xa1398a0));
+    address public constant cassie = address(uint160(0xca551e));
+    address public constant sofia = address(uint160(0x50F1a));
+    address public constant bob = address(uint160(0xb0b));
+    address public constant bia = address(uint160(0xb1a));
+    address public constant baboo = address(uint160(0xbab00));
 }
 // solhint-enable const-name-snakecase
